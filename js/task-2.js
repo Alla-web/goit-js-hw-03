c45be18c1cae2a64c6e6dd2a695bbd53ebd17f9e
Alla-Nazarenko <umnyj.start@gmail.com>
2025-08-22 15:24:09 +0200

    homework done

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let newArray = [];
+
+  if (firstArray.concat(secondArray).length < maxLength) {
+    return `Sorry, max length more then total length of both arrays`;
+  } else {
+    newArray = firstArray.concat(secondArray).slice(0, maxLength);
+  }
+
+  //   або
+  //   for (let i = 0; i < maxLength; i++) {
+  //     newArray.push(firstArray.concat(secondArray)[i]);
+  //   }
+
+  return newArray;
+}
+
+console.log("-------------------Task 2-----------------------");
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(" ");
